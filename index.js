93b7c5375f037674ac4c7204761303a84807edf2
unknown <katoukanehito@yahoo.co.jp>
2026-05-25 13:28:03 +0900

    挨拶関数を実装した

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
 function greet (){
   console.log("Hello from the remote blanch")
 }
+=======
+function greet() {
+    console.log("Hello from the local branch");
+}
+>>>>>>> 3b295a0 (挨拶関数を実装した)
